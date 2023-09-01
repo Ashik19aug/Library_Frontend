691dd751339bda71875cc1c68b5a4667fa2652fb
Ashik19aug <ashik19aug@gmail.com>
2023-09-01 18:08:18 +0600

    remove unused style

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,6 @@
 import styles from "./style";
-import {useEffect, useState} from "react";
 
 const App = () => {
-  const [theme, setTheme] = useState(null);
-  useEffect(() => {
-    if(window.matchMedia('(prefers-color-scheme:dark)'),matches){
-
-    }
-  }, []);
   return (
       <div className={`${styles.PrimaryBgDark}`}>
         <button className="rounded-full">Save Changes</button>
